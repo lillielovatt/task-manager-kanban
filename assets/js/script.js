b66ac13a6995286b902bcbff957df464c3c5c7f4
lillielove <lillielove@gmail.com>
2022-05-23 16:47:42 -0700

    enable user to click OR enter in order to submit new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // The El suffix identifies this as a DOM element; this naming convention will help us keep track of which variables store DOM elements.
@@ -6,12 +6,15 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // var taskListEl = document.querySelector(".task-list");
 // vartaskListEl = document.querySelector("#id-for-ul");
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+    event.preventDefault();
+    
     var taskItemEl = document.createElement("li");
     taskItemEl.className="task-item";
     taskItemEl.textContent="This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener("submit", createTaskHandler);
+// using submit allows us to either click OR enter key
+// using "click" removes the enter key option
