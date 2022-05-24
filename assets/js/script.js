3c77e844a6057c3eee6ebbb946e02833f0999a61
lillielove <lillielove@gmail.com>
2022-05-23 17:59:05 -0700

    add to create task function, allow name and type of task to display by modifying HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,27 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event){
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className="task-item";
-    taskItemEl.textContent="This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl);
+    // create list
+    var listItemEl = document.createElement("li");
+    listItemEl.className="task-item";
+    
+    // create div
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className ="task-info";
+
+    // add HTML to div, then to list
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput +  "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // adds list to ul
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
+
 // using submit allows us to either click OR enter key
 // using "click" removes the enter key option
